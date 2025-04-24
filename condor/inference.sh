d08222f9b897b8879446b307e8da07ac10617822
ruveydayilmaz0 <59517700+ruveydayilmaz0@users.noreply.github.com>
2025-04-24 15:23:25 +0200

    Delete condor directory

--- condor/inference.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd /work/scratch/yilmaz/ControlNet
-/work/scratch/yilmaz/miniconda3/envs/control/bin/python inference/inference_hela.py
--- condor/train.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd /work/scratch/yilmaz/ControlNet
-/work/scratch/yilmaz/miniconda3/envs/control/bin/python train/train_cells_cho.py
